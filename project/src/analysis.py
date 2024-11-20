5131b6fb5b190b7f2c4eb1b1d45a3f06f7e34e77
Kedsson Figueredo <kedsson19@gmail.com>
2024-11-19 21:26:22 -0300

    update analysis.py and main.py

--- project/src/analysis.py
+++ project/src/analysis.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
 
-def plot_student_performance(student_data, student_id):
+def analyze_data(student_data, student_id):
     """
-    Gera o gráfico de pizza com o desempenho do aluno por trilha.
+    Gera um gráfico de pizza com o desempenho do aluno por trilha,
+    garantindo que cada trilha seja agregada de forma única.
     """
-    if not student_data.empty:
-        labels = student_data['trail']
-        sizes = student_data['correct_answers'] / student_data['total_questions'] * 100
+    filtered_data = student_data[student_data['student_id'] == student_id]
+    
+    if not filtered_data.empty:
+        # Agrupa os dados por trilha e calcula a média das respostas corretas e do total de perguntas
+        grouped_data = filtered_data.groupby('trail').sum()
+        grouped_data['percentage'] = grouped_data['correct_answers'] / grouped_data['total_questions'] * 100
+        
+        # Cria as etiquetas e os tamanhos para o gráfico de pizza
+        labels = grouped_data.index
+        sizes = grouped_data['percentage']
 
+        # Gera o gráfico de pizza
         plt.figure(figsize=(6, 6))
         plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
         plt.title(f'Desempenho do Aluno {student_id} nas Trilhas')
--- project/src/main.py
+++ project/src/main.py
@@ -9,17 +9,23 @@ from src.preprocess import preprocess_data
 from src.analysis import analyze_data
 
 def main():
-    # Caminho do arquivo Excel
-    file_path = "project/data/train/student_performance_base_data.xlsx"
+
+    file_path = "../project/data/train/student_performance_base_data.xlsx"
     
+    # Verifica se o ficheiro existe
+    if os.path.isfile(file_path):
+        print("Ficheiro encontrado!")
+    else:
+        print("Ficheiro não encontrado!")
+        return  # Termina a execução se o ficheiro não for encontrado
+
     # Carregar os dados
     data = load_data(file_path)
     
     # Pré-processar os dados
     preprocessed_data = preprocess_data(data)
-    
     # Realizar a análise
-    analyze_data(preprocessed_data)
+    analyze_data(preprocessed_data, 1)
 
 if __name__ == "__main__":
     main()
